6b6ad03b0499375a5ba92796b5b070e04e112934
Fabiano Oliveira <102564420+literallyfabin@users.noreply.github.com>
2022-04-30 18:54:25 -0300

    atvd

---
+++ Ex005/ex001.js
@@ -0,0 +1,5 @@
+var velocidade = '61'
+if (velocidade > 60){
+    console.log(`Você foi multado pois sua velocidade ultrapassou o limite de 60 e passou a ser ${velocidade}!`)
+}
+console.log('Dirija sempre usando o sinto de segurança!')
---
+++ Ex005/ex002.js
@@ -0,0 +1,8 @@
+var pinto = 'goldao'
+console.log(`Quem é você, para saber qual o pinto? ${pinto}`)
+if(pinto != 'goldao'){
+    console.log('Você não é o goldao, então seu pinto é mediano!')
+
+}else{
+    console.log('Esse pinto é gigantesco!')
+}
