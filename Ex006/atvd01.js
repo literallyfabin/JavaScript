56af5b813a90ee9e6d024d130576db182bf28af5
Fabiano Oliveira <102564420+literallyfabin@users.noreply.github.com>
2022-05-01 21:57:52 -0300

    atvd

---
+++ Ex006/atvd01.js
@@ -0,0 +1,18 @@
+var idade = '30'
+if (idade >= 18 && idade <=29){   
+    console.log('Você é Maior de idade !')
+
+     
+}  else{
+    if(idade > 13 && idade <= 15){
+        console.log('Você está na pré-adolescencia!')
+    } 
+    if(idade < 13){
+        console.log('Você tem idade abaixo dos 14 anos !')
+        
+    }
+        if(idade >=30){
+            console.log('Você está na terceira idade !')
+        }
+    
+}
